405e44b0b8e7365ba86c1419e28ababa5b3c245a
Tobias Nyholm <tobias.nyholm@gmail.com>
2021-08-30 14:48:02 -0700

    Adding more tests to ensure correctness (#3)
    * Added tests for getters
    
    * CS

--- tests/NanoKernelTest.php
+++ tests/NanoKernelTest.php
@@ -4,14 +4,47 @@ declare(strict_types=1);
 
 namespace Nyholm\NanoKernel\Tests;
 
+use Example1\App\Kernel;
 use Nyholm\NanoKernel\NanoKernel;
 use PHPUnit\Framework\TestCase;
 
 class NanoKernelTest extends TestCase
 {
+    public static function setUpBeforeClass(): void
+    {
+        parent::setUpBeforeClass();
+
+        include_once __DIR__.'/Resources/app/src/Kernel.php';
+    }
+
     public function testCanBeInitialized()
     {
         $kernel = new NanoKernel('dev', true);
         $this->assertInstanceOf(NanoKernel::class, $kernel);
+        $kernel = new NanoKernel('dev', false);
+        $this->assertInstanceOf(NanoKernel::class, $kernel);
+        $kernel = new NanoKernel('prod', false);
+        $this->assertInstanceOf(NanoKernel::class, $kernel);
+    }
+
+    public function testGetters()
+    {
+        $kernel = new class('dev', true) extends NanoKernel {
+            public function getProjectDir(): string
+            {
+                return 'project-dir';
+            }
+        };
+
+        $this->assertSame('project-dir/var/log', $kernel->getLogDir());
+        $this->assertSame('project-dir/var/cache/dev', $kernel->getCacheDir());
+        $this->assertSame('project-dir/var/cache/dev', $kernel->getBuildDir());
+        $this->assertSame('project-dir/config', $kernel->getConfigDir());
+    }
+
+    public function testGetProject()
+    {
+        $kernel = new Kernel('dev', true);
+        $this->assertSame(__DIR__.'/Resources/app', $kernel->getProjectDir());
     }
 }
---
+++ tests/Resources/app/src/Kernel.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Example1\App;
+
+use Nyholm\NanoKernel\NanoKernel;
+
+class Kernel extends NanoKernel
+{
+}
